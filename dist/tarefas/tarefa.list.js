1a478a2fe00127c960e4508c36118e28abe6d079
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-08-18 01:06:28 -0300

    excluindo tarefas

--- dist/tarefas/tarefa.list.js
+++ dist/tarefas/tarefa.list.js
@@ -22,11 +22,20 @@ class TarefaPaginaListagem {
             btnEditar.innerText = "Editar";
             btnEditar.className = "btn btn-primary";
             btnEditar.addEventListener("click", () => {
-                const idSelecionado = novaLinha.cells[0].innerText;
+                const idSelecionado = tarefa.id;
                 //query parameter
                 window.location.href = `tarefa.create.html?id=${idSelecionado}`;
             });
+            const btnExcluir = document.createElement("a");
+            btnExcluir.innerText = "Excluir";
+            btnExcluir.className = " m-1 btn btn-danger";
+            btnExcluir.addEventListener("click", () => {
+                const idSelecionado = tarefa.id;
+                this.repositorioTarefas.excluir(idSelecionado);
+                window.location.reload();
+            });
             celulaBotoes.appendChild(btnEditar);
+            celulaBotoes.appendChild(btnExcluir);
         });
     }
 }
